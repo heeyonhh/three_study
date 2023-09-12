0ddee185c6c165f42f2529d0c974a5b89d052ddf
heeyonhh <lightram23@gmail.com>
2023-09-12 21:32:37 +0900

    01basic3

--- 01_basic.js
+++ 01_basic.js
@@ -29,6 +29,47 @@ class App{
         //render 메소드(3차원 그래픽을 만들어주는 장면을 만들어주는)를 requestAnimationFrame api에 넘겨 호출
         requestAnimationFrame(this.render.bind(this));
     }
+
+    //카메라 객체 생성
+    //3차원 그래픽을 출력할 영역에 대한 가로 세로 크기 얻어옴
+    _setupCamera(){
+        const width = this._divContainer.clientWidth;
+        const height = this._divContainer.clientHeight;
+        const camera = new THREE.PerspectiveCamera(
+            75,
+            width / height,
+            0.1,
+            100
+        );
+        camera.getWorldPosition.z = 2;
+        this._camera = camera;
+        //또다른 메소드에서 사용할수 있도록 정의
+    }
+
+    //광원 생성
+    _setupLight(){
+        const color = 0xffffff;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        //광원의 위치
+        light.position.set(-1, 2, 4);
+        //광원을 scene 객체의 구성 요소로 추가
+        this._scene.add(light);
+    }
+
+    //파란색 계열 3d mesh 생성
+    //BoxGeometry(가로, 세로, 깊이)
+    _setupModel(){
+        const geometry = new THREE.BoxGeometry(1, 1, 1);
+        const material = new THREE.MeshPhongMaterial({color: 0x44a88});
+
+        const cube = new THREE.Mesh(geometry, material);
+
+        //mesh를 scene 객체의 구성 요소로 추가
+        this._scene.add(cube);
+        //다른 메소드에서 참조 될수 있도록 필드화
+        this._cube = cube;
+    }
 }
 
 window.onload = function() {
