7309224ef466ae6bc1a220ae1eb8da896c6fa561
heeyonhh <lightram23@gmail.com>
2023-10-07 16:48:00 +0900

    commit

--- 02_geometry.js
+++ 02_geometry.js
@@ -31,38 +31,51 @@ class App {
     new OrbitControls(this._camera, this._divContainer);
   }
 
+  //points 좌표들이 이루는 선
   _setupModel() {
-    class CustomSincurve extends THREE.Curve {
-      constructor(scale) {
-        super();
-        this.scale = scale;
-      }
-      getPoint(t){
-        const tx = t * 3 - 1.5;
-        const ty = Math.sin(2 * Math.PI * t);
-        const tz = 0;
-        return new THREE.Vector3(tx, ty, tz).multiplyScalar(this.scale);
-      }
+    const points = [];
+    for (let i = 0; i < 10; ++i){
+      points.push(new THREE.Vector2(Math.sin(i * 0.2) * 3 + 3, (i - 5) * .8));
     }
 
-    const path = new CustomSincurve(4);
-    const geometry = new THREE.TubeGeometry(path, 40, 0.8, 8, true);
+    const geometry = new THREE.BufferGeometry();
+    geometry.setFromPoints(points);
 
-    const fillMaterial = new THREE.MeshPhongMaterial({ color: 0x515151 });
-    const cube = new THREE.Mesh(geometry, fillMaterial);
+    const material = new THREE.LineBasicMaterial({ color: 0xffff00 });
+    const line = new THREE.Line(geometry, material);
 
-    const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffff00 });
-    const line = new THREE.LineSegments(
-      new THREE.WireframeGeometry(geometry),
-      lineMaterial
-    );
+    this._scene.add(line);
+    // class CustomSincurve extends THREE.Curve {
+    //   constructor(scale) {
+    //     super();
+    //     this.scale = scale;
+    //   }
+    //   getPoint(t){
+    //     const tx = t * 3 - 1.5;
+    //     const ty = Math.sin(2 * Math.PI * t);
+    //     const tz = 0;
+    //     return new THREE.Vector3(tx, ty, tz).multiplyScalar(this.scale);
+    //   }
+    // }
 
-    const group = new THREE.Group();
-    group.add(cube);
-    group.add(line);
+    // const path = new CustomSincurve(4);
+    // const geometry = new THREE.TubeGeometry(path, 40, 0.8, 8, true);
 
-    this._scene.add(group);
-    this._cube = group;
+    // const fillMaterial = new THREE.MeshPhongMaterial({ color: 0x515151 });
+    // const cube = new THREE.Mesh(geometry, fillMaterial);
+
+    // const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffff00 });
+    // const line = new THREE.LineSegments(
+    //   new THREE.WireframeGeometry(geometry),
+    //   lineMaterial
+    // );
+
+    // const group = new THREE.Group();
+    // group.add(cube);
+    // group.add(line);
+
+    // this._scene.add(group);
+    // this._cube = group;
   }
 
   _setupCamera() {
