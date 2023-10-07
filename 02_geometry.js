98e70827aa18e7289ae49d0a198f7a55407ce014
heeyonhh <lightram23@gmail.com>
2023-10-07 17:17:26 +0900

    FontLoader TextGeometry

--- 02_geometry.js
+++ 02_geometry.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'OrbitControls';
 //마우스 회전 컨트롤
+import { FontLoader } from "../examples/jsm/loaders/FontLoader.js"
+import { TextGeometry } from "../examples/jsm/geometries/TextGeometry.js"
 
 class App {
   constructor() {
@@ -31,30 +33,45 @@ class App {
     new OrbitControls(this._camera, this._divContainer);
   }
 
-  //points 좌표들이 이루는 선 Lathe 토기 같이 생긴..
   _setupModel() {
-    const points = [];
-    for (let i = 0; i < 10; ++i){
-      points.push(new THREE.Vector2(Math.sin(i * 0.2) * 3 + 3, (i - 5) * .8));
-    }
+    const fontLoader = new FontLoader();
+    async function loadFont(that){
+      const url = "../examples/fonts/helvetiker_regular.typeface.json";
+      const font = await new Promise((resolve, reject) => {
+        fontLoader.load(url, resolve, undefined, reject);
+      });
 
-    const geometry = new THREE.LatheGeometry(points, 32, 0, Math.PI);
+      const geometry = new TextGeometry("GIS", {
+        font: font, // fontLoader를 통해서 얻어온 객체
+        size: 9, // 텍스트 메쉬의 크기이다. 기본값은 100
+        height: 1.8, // 깊이 값이다. 기본값은 50
+        curveSegments: 5, // 하나의 커브를 구성하는 정점의 갯수이다. 기본값은 12
+        // setting for ExtrudeGeometry
+        bevelEnabled: true, // 베벨링 처리를 할 것인지의 여부. 기본값은 true
+        bevelThickness: 1.5, // 베벨링에 대한 두께 값이다. 기본값은 6이다.
+        bevelSize : 1.7,  // shape의 외곽선으로부터 얼마나 멀리 베벨링 할 것인지에 대한 거리. 기본값은 2
+        bevelOffset : 0,  // 텍스트 윤곽선 베벨에서 시작하는 거리이다. * 이 값을 반드시 지정
+        bevelSegments: 3 // 베벨링 단계 수. 기본값은 3이다.
+      });
 
-    const fillMaterial = new THREE.MeshPhongMaterial({ color: 0x515151 });
-    const cube = new THREE.Mesh(geometry, fillMaterial);
+      const fillMaterial = new THREE.MeshPhongMaterial({ color: 0x515151 });
+      const cube = new THREE.Mesh(geometry, fillMaterial);
+  
+      const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffff00 });
+      const line = new THREE.LineSegments(
+        new THREE.WireframeGeometry(geometry),
+        lineMaterial
+      );
+  
+      const group = new THREE.Group();
+      group.add(cube);
+      group.add(line);
+  
+      that._scene.add(group);
+      that._cube = group;
 
-    const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffff00 });
-    const line = new THREE.LineSegments(
-      new THREE.WireframeGeometry(geometry),
-      lineMaterial
-    );
-
-    const group = new THREE.Group();
-    group.add(cube);
-    group.add(line);
-
-    this._scene.add(group);
-    this._cube = group;
+    };
+    loadFont(this);
   }
 
   _setupCamera() {
